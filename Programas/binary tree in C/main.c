a85b0146d7248fb747ddc0726cfbf9b9dd5a92b1
RICARDO <ricardomarquesddsa@gmail.com>
2025-05-23 16:36:29 -0300

    Renames

--- Programas/binary tree in C/main.c
+++ Programas/binary tree in C/main.c
@@ -3,24 +3,24 @@
 #include "bintre.h"
 
 int main(int argc, char const *argv[]){
-    bin_t bina[4]; // Minhas informações bases para formar a pilha
-    bin_t *root = &bina[(sizeof(bina)/sizeof(bin_t))/2]; // A pilha em si, não inicializada
+    bin_t data[4]; // Minhas informações bases para formar a pilha
+    bin_t *tree = &data[(sizeof(data)/sizeof(bin_t))/2]; // A pilha em si, não inicializada
     int var = 30;
 
     // Os dados brutos
-    bina[0].value = 1;
-    bina[1].value = 4;
-    bina[2].value = 10;
-    bina[3].value = 21;
-    inibin(bina, 4); // Cria a pilha
+    data[0].value = 1;
+    data[1].value = 4;
+    data[2].value = 10;
+    data[3].value = 21;
+    inibin(data, 4); // Cria a pilha
 
     // Acessando a pilha    
-    printf("Valor do poteiro: %d\n", root->value);
-    printf("Ponteiro esquerdo: %d\n", root->left);
-    printf("Ponteiro direito: %d\n\n", root->right);
+    printf("Valor do poteiro: %d\n", tree->value);
+    printf("Ponteiro esquerdo: %d\n", tree->left);
+    printf("Ponteiro direito: %d\n\n", tree->right);
 
     // Testando a busca binária
-    printf("Valor encontrado(certeiro ou aproximado) na busca binária: %d\n\n", binsearch(root, var));
+    printf("Valor encontrado(certeiro ou aproximado) na busca binária: %d\n\n", binsearch(tree, var));
 
     return 0;
 }
